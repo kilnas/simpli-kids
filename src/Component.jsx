5f7211531a0ca68e01c5757fb0e08baa089cfcfe
RJBowen <rjbowen97@gmail.com>
2018-06-28 15:16:14 -0400

    Implemented a popUp

--- src/Component.jsx
+++ src/Component.jsx
@@ -1,26 +1,30 @@
 import React from 'react';
 import HomePage from './pages/HomePage.js'
 import PopUp from "./PopUp";
+import autobind from "auto-bind"
 
-var Component = React.createClass({
-  getInitialState: function() {
-    return {message: 'Hello!'};
-  },
 
-  render: function () {
-    if (this.props.onRender) {
-      this.props.onRender();
+export default class Component extends React.Component{
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      text: "Hello there",
+      showEventPopUp: true
     }
+
+    autobind(this)
+  }
+
+  render() {
     return (
       <div className="container">
         <div className="jumbotron">Simpli Kids</div>
         <HomePage />
+        {this.state.showEventPopUp && <PopUp text={this.state.text} />}
       </div>
     );
-  },
-  propTypes: {
-    onRender: React.PropTypes.func
   }
-});
 
-export default Component;
+};
--- src/PopUp.jsx
+++ src/PopUp.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import styles from './PopUp.css'
+import autobind from "auto-bind"
 
 export default class PopUp extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            text: this.props.text || "default value",
+        }
+
+        autobind(this)
+    }
+    
     render() {
         return (
             <span className="popUp">
-                Hello there!
+                {this.state.text}
             </span>     
         );
     }
